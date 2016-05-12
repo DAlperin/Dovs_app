67d35998971ab28db050f4e55cfeec5c55ec8f35
DAlperin <dzalperin@gmail.com>
2016-05-12 17:49:53 -0400

    Added playground

---
+++ Dovs_playground.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var message = "Hello world the total is: "
+
+print(message)
+
+var a = 1
+var b = 10
+
+var total = a + b
+
+print(message, total)
+
+if (total < 100) {
+    print("YAY!!!")
+    
+    let alertController = UIAlertController(title: "Dov's App Other button", message:
+        "Go to browser?", preferredStyle: UIAlertControllerStyle.Alert)
+    
+    let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) {
+        UIAlertAction in
+                //if ok from the popup alert is clicked
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) {
+        UIAlertAction in
+       
+    }
+}
+
