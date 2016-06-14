c3abdc535d8453a1b971b99de55255db65476625
DAlperin <dzalperin@gmail.com>
2016-06-14 11:14:54 -0400

    Just changed some project settings changed system version.

--- Dovs_playground.playground/Contents.swift
+++ Dovs_playground.playground/Contents.swift
@@ -14,7 +14,25 @@ var total = a + b
 
 print(message, total)
 
-if (total < 100) {
+if (total < 50) {
+    print("hello")
+}
+print(str, "text ", message)
+
+var bob = "Bobby bob bob"
+var blob = "Blobby blob blob"
+
+print(bob)
+
+var y = 2
+var z = 20
+
+var arby = y - z
+
+print(arby, blob, bob)
+
+
+/*if (total < 100) {
     print("YAY!!!")
     
     let alertController = UIAlertController(title: "Dov's App Other button", message:
@@ -31,5 +49,6 @@ if (total < 100) {
     }
     
     
-}
+} 
+ */
 
